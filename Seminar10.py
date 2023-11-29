173e1a3b445f366a1a8e01abbc675f1292b15aaf
KirillBunakov <lonley987@gmail.com>
2023-11-29 11:42:09 +0300

    добавил решение

--- Seminar10.py
+++ Seminar10.py
@@ -6,3 +6,9 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 data.head()
+
+unique_values = data['whoAmI'].unique()
+one_hot_view = pd.DataFrame()
+
+for value in unique_values:
+    one_hot_view[value] = (data['whoAmI'] == value).astype(int)
